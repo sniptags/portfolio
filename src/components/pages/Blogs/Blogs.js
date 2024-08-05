6db06cc3d8c18ad7d4aab475dc67f872c9e7f19e
Gagandeep Singh <77214044+sniptags@users.noreply.github.com>
2024-08-05 17:23:41 -0600

    Revert "added outlet in router to reslove nested routing"

--- src/components/pages/Blogs/Blogs.js
+++ src/components/pages/Blogs/Blogs.js
@@ -2,25 +2,21 @@ import Layout from "../../layout/Layout";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Outlet } from "react-router-dom";
 const Blogs = ()=>{
     return(
-        <>
-            <Layout>
-                <Container>
-                    <Row>
-                        <Col lg={6}>
-                            <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7192747799401107456" height="974" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>   
-                        </Col>
-                        <Col lg={6}>
-                        <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7096641689817153536" height="974" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
-                        </Col>
-                    </Row>
-                </Container>
-                
-            </Layout>
-            <Outlet />
-        </>
+        <Layout>
+            <Container>
+                <Row>
+                    <Col lg={6}>
+                         <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7192747799401107456" height="974" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>   
+                    </Col>
+                    <Col lg={6}>
+                    <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7096641689817153536" height="974" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                    </Col>
+                </Row>
+            </Container>
+            
+        </Layout>
     )
 }
 
--- src/components/pages/ComingSoon/ComingSoon.js
+++ src/components/pages/ComingSoon/ComingSoon.js
@@ -2,23 +2,20 @@ import { Container,Row,Col } from "react-bootstrap";
 import Layout from "../../layout/Layout";
 import { useContext } from "react";
 import ThemeContext from "../../../store/theme-context";
-import { Outlet } from "react-router-dom";
+
 function ComingSoon() {
     const themeCtx = useContext(ThemeContext);
     const isDarkMode = themeCtx.isDarkMode
     return (
-        <>
-            <Layout>
-                <section className={`${isDarkMode ? "bg-dark text-light": "text-secondary"} pb-5`}>
-                    <Container>
-                        <Row>
-                            <Col lg={12}><div className="text-center"><h1>Stay Tuned, This page is under construction</h1></div></Col>
-                        </Row>
-                    </Container>
-                </section>
-            </Layout>
-            <Outlet />
-        </>
+        <Layout>
+            <section className={`${isDarkMode ? "bg-dark text-light": "text-secondary"} pb-5`}>
+                <Container>
+                    <Row>
+                        <Col lg={12}><div className="text-center"><h1>Stay Tuned, This page is under construction</h1></div></Col>
+                    </Row>
+                </Container>
+            </section>
+        </Layout>
     )
 
   }
